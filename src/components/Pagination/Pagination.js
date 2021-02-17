7d0111865a93e4b339879e415b256e763dd8cb0a
Alexander Levonyuk <saga2002@gmail.com>
2021-02-17 07:22:05 +0300

    improved pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,3 +1,4 @@
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPageSearchData, fetchMovies } from "../../store/actionCreators/action";
@@ -7,12 +8,14 @@ import "./Pagination.css";
 const Pagination = ({ totalResults }) => {
   const dispatch = useDispatch();
   const searchData = useSelector((state) => state.movies.searchData);
+  const currentPage = Number(useSelector((state) => state.movies.searchData.page));
   const pageSize = 10;
 
   const pagesCount = Math.ceil(+totalResults / pageSize);
-  const pages = [];
+  const allPages = [];
+  const pageRangeDisplayed = 5;
   for (let i = 1; i <= pagesCount; i++) {
-    pages.push(i);
+    allPages.push(i);
   }
 
   const setCurrentPage = (e) => {
@@ -21,15 +24,70 @@ const Pagination = ({ totalResults }) => {
     dispatch(fetchMovies({ title: searchData.title, year: searchData.year, page }));
   };
 
-  const listElements = pages.map((p) => {
-    return (
-      <li className={+searchData.page === p ? "active" : ""} key={p} onClick={setCurrentPage}>
-        {p}
-      </li>
-    );
-  });
+  const nextPage = () => {
+    const page = +searchData.page;
+    if (page < pagesCount) {
+      dispatch(setPageSearchData(page + 1));
+      dispatch(fetchMovies({ title: searchData.title, year: searchData.year, page }));
+    }
+  };
 
-  return <ul className="pagination">{listElements}</ul>;
+  const prevPage = () => {
+    const page = +searchData.page;
+    if (page > 1) {
+      dispatch(setPageSearchData(page - 1));
+      dispatch(fetchMovies({ title: searchData.title, year: searchData.year, page }));
+    }
+  };
+
+  const elementsPages = allPages
+    .slice(
+      currentPage < pageRangeDisplayed
+        ? 0
+        : currentPage < allPages.length - 3
+        ? currentPage - pageRangeDisplayed + 3
+        : allPages.length - pageRangeDisplayed,
+      currentPage < pageRangeDisplayed
+        ? pageRangeDisplayed
+        : currentPage < allPages.length - 3
+        ? currentPage + 1
+        : allPages.length
+    )
+    .map((p) => {
+      return (
+        <li className={currentPage === p ? "active" : ""} key={p} onClick={setCurrentPage}>
+          {p}
+        </li>
+      );
+    });
+
+  const ellipsis = <li>&#8230;</li>;
+  const startPage =
+    currentPage > 4 ? (
+      <React.Fragment>
+        <li onClick={setCurrentPage}>{allPages[0]}</li>
+        {ellipsis}
+      </React.Fragment>
+    ) : null;
+  const lastPage =
+    currentPage <= allPages.length - 4 ? (
+      <React.Fragment>
+        {ellipsis}
+        <li onClick={setCurrentPage}>{allPages.length}</li>
+      </React.Fragment>
+    ) : null;
+  const prev = <li onClick={prevPage}>&#8249;</li>;
+  const next = <li onClick={nextPage}>&#8250;</li>;
+
+  return (
+    <ul className="pagination">
+      {prev}
+      {startPage}
+      {elementsPages}
+      {lastPage}
+      {next}
+    </ul>
+  );
 };
 
 export default Pagination;
