f71ae0d575fab0f94cd3ff8d2b7c5fee0d59a0f7
Alexander Levonyuk <saga2002@gmail.com>
2021-03-01 07:21:44 +0300

    use scss

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { noChangeMovie } from "../../store/actionCreators/action";
 import noImg from "../Pictures/no_image_available.png";
-import s from "./Description.module.css";
+import "./Description.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowClose as close } from "@fortawesome/free-regular-svg-icons";
 
@@ -37,16 +37,16 @@ const Description = ({ changeMovie }) => {
       })
     : null;
   return (
-    <div className={s.container}>
-      <div className={s.description}>
+    <div className="container">
+      <div className="description">
         {changeMovie.Year && (
-          <FontAwesomeIcon onClick={() => dispatch(noChangeMovie())} className={s.close} size="2x" icon={close} />
+          <FontAwesomeIcon onClick={() => dispatch(noChangeMovie())} className="close" size="2x" icon={close} />
         )}
         {elementTitle}
         {elementDiscription}
       </div>
       {changeMovie.Poster && (
-        <div className={s.poster}>
+        <div className="poster">
           <img src={changeMovie.Poster !== "N/A" ? changeMovie.Poster : noImg} alt=""></img>
         </div>
       )}
